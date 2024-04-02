7a506997bd2874210f74b6f9fb468c799f2b5b5e
TahiraAnsari <122722822+TahiraAnsari@users.noreply.github.com>
2024-04-03 00:10:53 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+type anyType = {
+    userGuess: number,
+}
+const sytemGeneratedNo = Math.floor(Math.random()*10);
+//console.log(sytemGeneratedNo);
+const answers = await inquirer.prompt([
+{
+    type:"number",
+    name:"userGuess",
+    message:"Enter your Guessing number: ",
+}
+]);
+const {userGuess} = answers;
+if(userGuess === sytemGeneratedNo){
+    console.log(chalk.black(chalk.bgCyanBright(`\nYes, your answer is correct, You Win!! \n`)));
+    console.log(chalk.yellow(`You Guess the answer: ${answers.userGuess}\nSystem Generated Number is: ${sytemGeneratedNo}`));
+    
+}else{
+    console.log("Please try again better Luck, Next Time!!!");
+    console.log(chalk.yellow(`System Generated Number is ${sytemGeneratedNo} and You Guessing the number is ${answers.userGuess}`));
+    
+    
+}
+
